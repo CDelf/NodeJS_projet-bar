fa18bf95ffbf301cbc68b24f5fecf6cc31ac7345
Carole <c.delfino@live.fr>
2025-03-11 12:21:18 +0100

    correction model Bar.js

--- model/Bar.js
+++ model/Bar.js
@@ -3,10 +3,12 @@ const db = require("../config/db")
 const sequelize = require("sequelize")
 
 const Bar = db.define("bar", {
-        id: {type: sequelize.INTEGER, autoIncrement:true, primaryKey: true},
-        user_choice : {type : sequelize.ENUM('pierre, feuille, ciseaux')},
-        computer_choice : {type : sequelize.ENUM('pierre, feuille, ciseaux')},
-        result : {type : sequelize.ENUM('victoire, défaite, égalité')}
-    })
+    id: {type: sequelize.INTEGER, autoIncrement:true, primaryKey: true},
+    name : {type : sequelize.STRING, unique: true, allowNull: false},
+    adress : {type : sequelize.STRING, allowNull: false},
+    tel : {type : sequelize.STRING},
+    email : {type : sequelize.STRING, allowNull: false},
+    description : {type : sequelize.TEXT},
+})
 
 module.exports = Bar
